2369369fabe552095d1817f71a65028c7c192203
NadetDev <nadetdev@gmail.com>
2024-08-12 20:53:47 +0200

    app folder name change

--- app/server.js
+++ app/server.js
@@ -1,15 +1,18 @@
+import dotenv from 'dotenv';
 import express from 'express';
 
 const app = express();
-const PORT = 3000;
+dotenv.config();
+
+const PORT = process.env.SERVER_PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   res.status(200).send('Hello world');
-});
+}); */
 
 app.listen(PORT, () => {
   console.log(`Server is started at port ${PORT}`);
