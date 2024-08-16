f4809329691ac8dfb3ff43b5497d1251ec8c5967
NadetDev <nadetdev@gmail.com>
2024-08-16 02:53:01 +0200

    Add BMI + search apps

--- app/server.js
+++ app/server.js
@@ -34,6 +34,68 @@ app.get("/users", async (req, res) => {
   }, 2000);
 });
 
+app.post("/calculate-bmi", (req, res) => {
+  const height = parseFloat(req.body.height);
+  const weight = parseFloat(req.body.weight);
+
+  const randomId = Math.floor(Math.random() * 100);
+
+  if (height < 0 || height == 0) {
+    res.status(200).send(
+      `<p class='alert alert-danger' role='alert'>
+        height must be greater than 0!
+      </p>
+      `
+    );
+  } else {
+    const bmi = (weight / (height * height)).toFixed(2);
+
+    res.status(200).send(
+      `<p>#${randomId} Height of ${height} with weight of ${weight} gives your BMI of ${bmi}</p>
+    `
+    );
+  }
+});
+
+app.post("/users-search", async (req, res) => {
+  const searchTerm = req.body.search.toLowerCase();
+
+  if (!searchTerm) {
+    return res.send(`
+      <tr>No result</tr>
+      `);
+  }
+
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const usersList = await response.json();
+
+  const resultOfSearch = usersList.filter((user) => {
+    const name = user.name.toLowerCase();
+    const username = user.username.toLowerCase();
+    const email = user.email.toLowerCase();
+    return (
+      name.includes(searchTerm) ||
+      username.includes(searchTerm) ||
+      email.includes(searchTerm)
+    );
+  });
+
+  const resultToHtml = resultOfSearch
+    .map(
+      (user) => `
+       <tr>
+         <td>${user.name}</td>
+         <td>${user.username}</td>
+         <td>${user.email}</td>
+         <td></td>
+       </tr>
+    `
+    )
+    .join("");
+
+  res.status(200).send(resultToHtml);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is started at port ${PORT}`);
 });
